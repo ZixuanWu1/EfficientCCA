9fee07424b03c2baad0980d28fd404c90d087573
Zixuan Wu <115194385+ZixuanWu1@users.noreply.github.com>
2024-09-20 21:49:04 -0500

    Update launch_exp6.sh

--- R/launch_exp6.sh
+++ R/launch_exp6.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+# Define the values for the variables
+
+n_values="400"
+p_values="200"
+s_values="5 10 15"
+r_values="2 3 4 5"
+
+for n in $n_values; do
+for p in $p_values; do
+for s in $s_values; do
+for r in $r_values; do
+sbatch EfficientCCA/R/exp2.sh "$n" "$p" "$s" "$r"
+done
+done
+done
+done
